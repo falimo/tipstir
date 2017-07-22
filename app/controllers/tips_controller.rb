f8c4fea65f26bb3d2c4d1b686b0dea3cdb667fcb
Farouk Alimohamed <falimo@yahoo.com>
2017-07-22 17:37:07 +0000

    can add tips now through form

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -4,4 +4,19 @@ class TipsController < ApplicationController
     @tips = Tip.all    
   end
 
+  def new
+    @tip = Tip.new
+  end
+
+  def create
+    Tip.create(tip_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def tip_params
+    params.require(:tip).permit(:title, :description)
+  end
+
 end
